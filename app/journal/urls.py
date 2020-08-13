d6c38c3d113fa33b144720dbb9be873ddd9d5b0b
costa86 <costa86@zoho.com>
2020-08-13 16:06:22 +0100

    crud

--- app/journal/urls.py
+++ app/journal/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from . views import notes_all, note_new
+from . views import notes_all, note_add, note_edit, note_delete
 
 urlpatterns = [
     path('', notes_all, name="notes_all"),
-    path('note/new/', note_new, name="note_new")
-
+    path('note/add/', note_add, name="note_add"),
+    path('note/<int:pk>/edit/', note_edit, name="note_edit"),
+    path('note/<int:pk>/delete/', note_delete, name="note_delete")
 ]
--- app/journal/views.py
+++ app/journal/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . models import Note
 from . forms import NoteForm
 
+templates = {
+    "all": "journal/notes_all.html",
+    "add": "journal/note_add_edit.html",
+    "edit": "journal/note_add_edit.html"
+}
+
+
 def notes_all(request):
     records = Note.objects.all()
-    return render(request,"journal/notes_all.html",{"records":records})
+    return render(request, templates["all"], {"records": records})
 
-def note_new(request):
+
+def note_delete(request, pk):
+    record = get_object_or_404(Note, pk=pk)
+    record.delete()
+    return redirect("notes_all")
+
+
+def note_add(request):
     if request.method == "POST":
         form = NoteForm(request.POST)
         if form.is_valid():
@@ -16,4 +30,18 @@ def note_new(request):
             return redirect("notes_all")
     else:
         form = NoteForm()
-    return render(request,"journal/note_new.html",{"form":form})
+    return render(request, templates["add"], {"form": form})
+
+
+def note_edit(request, pk):
+    record = get_object_or_404(Note, pk=pk)
+    if request.method == "POST":
+        form = NoteForm(request.POST, instance=record)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.author = request.user
+            note.publish()
+            return redirect("notes_all")
+    else:
+        form = NoteForm(instance=record)
+    return render(request, templates["edit"], {"form": form})
