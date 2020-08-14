fd11ac0e7ab963a1d9490ddcc19fe7e15694e9e3
costa86 <costa86@zoho.com>
2020-08-14 14:55:20 +0100

    humanize

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -26,8 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     "journal",
-    'django_sass',
+    'django_sass'
 
 ]
 
