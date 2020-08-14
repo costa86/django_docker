065258058b8ada9e326283f5916c639d4ff0cf3c
costa86 <costa86@zoho.com>
2020-08-14 10:12:31 +0100

    gitignore

--- app/py3.sh
+++
@@ -1,7 +0,0 @@
-dir=/home/costa/Treasure/python
-
-cd ${dir}
-source py36/bin/activate
-
-
-notify-send "Py3 venv activated"
--- app/watch_sass.sh
+++
@@ -1,1 +0,0 @@
-python manage.py sass journal/static/scss/style.scss journal/static/css/style.css --watch
